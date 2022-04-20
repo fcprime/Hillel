9833cec63406bf81f663066689a791d718015714
Serhii Norov <Serhiinorov110@gmail.com>
2022-04-20 11:52:58 +0200

    homework

---
+++ script.js
@@ -0,0 +1,7 @@
+alert('Hello');
+let answerName = prompt('What is your name?');
+let userName = 'Nice to meet you, ' + answerName;
+alert(userName);
+let answerAge = prompt('How old are you?')
+let userAge = 2022 - answerAge;
+alert(userAge);
