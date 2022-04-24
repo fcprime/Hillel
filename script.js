adfe8f47ac30776fda2e7952d7ff5109a1b418f0
Serhii Norov <Serhiinorov110@gmail.com>
2022-04-24 17:51:01 +0200

    homework 22.04

--- script.js
+++ script.js
@@ -1,7 +1,28 @@
-alert('Hello');
-let answerName = prompt('What is your name?');
-let userName = 'Nice to meet you, ' + answerName;
-alert(userName);
-let answerAge = prompt('How old are you?')
-let userAge = 2022 - answerAge;
-alert(userAge);
+// alert('Hello');
+// let answerName = prompt('What is your name?');
+// let userName = 'Nice to meet you, ' + answerName;
+// alert(userName);
+// let answerAge = prompt('How old are you?')
+// let userAge = 2022 - answerAge;
+// alert(userAge);
+
+const enterFirstNumber = prompt('Enter first number');
+const enterSecondNumber = prompt('Enter second number');
+
+
+const threeNumber = +enterFirstNumber + +enterSecondNumber;
+const sum = enterFirstNumber + '+' + enterSecondNumber + '=' + threeNumber;
+console.log(sum);
+
+const minusNumber = +enterFirstNumber - +enterSecondNumber;
+const diff = enterFirstNumber + '-' + enterSecondNumber + '=' + minusNumber;
+console.log(diff);
+
+
+const multiplyNumber = +enterFirstNumber * +enterSecondNumber;
+const mult = enterFirstNumber + '*' + enterSecondNumber + '=' + multiplyNumber;
+console.log(mult);
+
+const divideNumber = +enterFirstNumber / +enterSecondNumber;
+const div = enterFirstNumber + '/' + enterSecondNumber + '=' + divideNumber;
+console.log(div);
